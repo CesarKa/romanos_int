36200c3d5e9fc1f93e010344d0191e271cd08801
César Redondo <cesarlmf8@gmail.com>
2024-12-12 22:10:56 +0100

    Programa roto y con falta de implementacion de casuistica de errores

--- romanos_int.py
+++ romanos_int.py
@@ -10,6 +10,9 @@ def romanos_int(signo):
 
 
 def traducir_romano(expresion):
+    valida, char = valida_repeticiones(expresion)
+    if not valida:    
+        raise ValueError(f"{char}Solo puede repetirse tres veces")
 
     total = 0
     n_previo = 0
@@ -26,9 +29,36 @@ def traducir_romano(expresion):
     return total
     
 
+def esta_en_racha(expresion,signo:str,racha:int)->bool:
+    cont = 0
+    if len(expresion) >= racha:
+        for el in expresion:
+            if el == signo:
+                cont += 1
+                if cont == racha:
+                    break
+            else:
+                cont = 0
+    else:
+        cont = 0
+    return cont == racha
+
+def valida_repeticiones(num_roman):
+    not_repeat = [("I", 4), ("X", 4), ("V", 2), ("D", 2), ("L", 2)
+                  ("M", 4), ("C", 4)]
+    result = True
+
+    for char in not_repeat:
+        if esta_en_racha(num_roman, char, 4):
+            result = False
+            break
+    return result, char
+
+
+
+
+
 expresion = input("Expresion romana que traducir: ")
-
-
 numero_traducido = traducir_romano(expresion) 
 print(f"Número traducido: {numero_traducido}")
 
--- test_romanos_decimal.py
+++ test_romanos_decimal.py
@@ -28,3 +28,45 @@ def traducir_romano():
     assert romanos_int("MCMXCIV") == 1994
     assert romanos_int("MMXXIII") == 2023
     assert romanos_int("MMMM") == 4000
+
+def test_validad_caracteres_romanos():
+    with pytest.raises(RomanNumberError) as contexto:
+        romanos_a_enteros("ZTW")
+    assert str(contexto.value).endswith("no es un simbolo romano")
+
+def test_validar_no_repeticiones_de_mas_de_1():
+    """
+    I, X, C, M hasta 3 veces
+    """
+    assert valida_repeticiones('IIII') == (False, "I", 4)
+    assert valida_repeticiones("XXXX") == (False, "X", 4)
+    assert valida_repeticiones("CCCC") == (False, "C", 4)
+    assert valida_repeticiones("MMMM") == (False, "M", 4)
+
+
+def test_validar_repeticiones_no_mas_de_3():
+    """
+    V, L, D no se pueden repetir
+    """
+    assert valida_repeticiones("VV") == (False, "V", 2)
+    assert valida_repeticiones("DD") == (False, "D", 2)
+    assert valida_repeticiones("LL") == (False, "L", 2)
+
+
+def test_validar_romano_cuatro_repeticiones():
+    with pytest.raises(RomanNumberError) as contexto:
+        romanos_a_enteros("MCCCCXXII")
+    assert str(contexto.value) == "C solo puede repetirse tres veces"
+
+
+def test_validar_romano_cuatro_repeticiones():
+    with pytest.raises(RomanNumberError) as contexto:
+        romanos_a_enteros("MCCVV")
+    assert str(contexto.value) == "V no puede repetirse"
+
+def test_restas_incorrectas():
+    with pytest.raises(RomanNumberError):
+        romanos_a_enteros("IC")
+
+    with pytest.raises(RomanNumberError):
+        romanos_a_enteros("VX")
